5faa6947a8addafb055b412145dfcae8fe99ba34
hui793114465 <hui793114465@163.com>
2016-09-07 16:46:26 +0800

    添加实例

---
+++ androidformdui/src/com/android/mdui/LayoutActivity.java
@@ -0,0 +1,57 @@
+package com.android.mdui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.mdui.layout.LayoutActivity1;
+
+public class LayoutActivity extends Activity implements OnItemClickListener{
+
+private ListView listView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		listView = new ListView(this);
+		listView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_expandable_list_item_1, getData()));
+		setContentView(listView);
+		listView.setOnItemClickListener(this);
+	}
+	
+	private List<String> getData(){
+        List<String> data = new ArrayList<String>();
+        data.add("layout1");
+        return data;
+    }
+	
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		switch (arg2) {
+		case 0:
+			startAnimActivity(LayoutActivity1.class);
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void startAnimActivity(Class<?> cla) {
+		this.startActivity(new Intent(this, cla));
+	}
+}
